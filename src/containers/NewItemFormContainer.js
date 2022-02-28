77d0be6655749994a52c36fc93cc408ccc9b6768
Muslimin Ontong <57244338+moshOntong-IT@users.noreply.github.com>
2022-02-28 18:03:28 +0800

    Adding Another Dispatch Solution
    I add comments for another way to add dispatch function

--- src/containers/NewItemFormContainer.js
+++ src/containers/NewItemFormContainer.js
@@ -6,6 +6,16 @@ const mapDispatchToProps = {
   onSubmit: addNewItem
 };
 
+//* Solution 2
+// const mapDispatchToProps = {
+//   onSubmit: (name,price) => addNewItem(name,price)
+// };
+
+//* Solution 2
+// const mapDispatchToProps = (dispatch)=>{
+//   onSubmit: (name,price) => dispatch(addNewItem(name,price))
+// };
+
 export const NewItemFormContainer = connect(
   null,
   mapDispatchToProps
